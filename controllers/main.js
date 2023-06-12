619b587a19489876282b5dfa85fa18f273a4d838
Tiago Rafael <tiagorafael777@gmail.com>
2023-06-12 16:17:06 -0400

    feat - jwt basics finish

---
+++ controllers/main.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const { BadRequestError } = require("../errors");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  // mongoose validations
+  // joi
+  // check in the controller
+
+  if (!username || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const id = new Date().getDate();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "user created", token });
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
+
+module.exports = { login, dashboard };
